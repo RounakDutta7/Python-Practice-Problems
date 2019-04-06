14acd15531736f7e57e5fd3377d6b287490d233b
Rounak Dutta <45180282+RounakDutta7@users.noreply.github.com>
2019-04-06 12:03:17 +0530

    Problem 2
    Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.
    
    Extras:
    1.    If the number is a multiple of 4, print out a different message.
    2.    Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.

--- prac2.py
+++ prac2.py
@@ -2,7 +2,7 @@
 """
 Created on Sat Apr  6 11:34:39 2019
 
-@author: rounak
+@author: rounakd
 Ask the user for a number. Depending on whether the number is even or odd,
 print out an appropriate message to the user.
 
