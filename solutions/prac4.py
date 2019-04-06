be52096acb4f7843bc83fd76f9ec580a2c6d0af2
Rounak Dutta <45180282+RounakDutta7@users.noreply.github.com>
2019-04-06 19:50:28 +0530

    Update prac4.py

--- solutions/prac4.py
+++ solutions/prac4.py
@@ -7,6 +7,8 @@ Created on Sat Apr  6 19:42:18 2019
 
 num = int (input("Enter a number: "))
 
+#if the elements in the range(2, num) evenly divides the num,
+#then it is included in the divisors list
 divisor = [x for x in range(2, num) if num % x == 0]
 
 for x in divisor:
